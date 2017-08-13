dd863ecb9c147cebf58338bf46b018d628f347e3
williamtwobit <william@twobit.cc>
2017-08-13 13:04:50 -0400

    hello world up

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,51 +1,21 @@
 import React from 'react';
-import * as Cookies from 'js-cookie';
-
-import QuestionPage from './question-page';
-import LoginPage from './login-page';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Home from './home';
+import {connect} from 'react-redux';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentUser: null
-        };
-    }
-
-    componentDidMount() {
-        // Job 4: Redux-ify all of the state and fetch calls to async actions.
-        const accessToken = Cookies.get('accessToken');
-        if (accessToken) {
-            fetch('/api/me', {
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            }).then(res => {
-                if (!res.ok) {
-                    if (res.status === 401) {
-                        // Unauthorized, clear the cookie and go to
-                        // the login page
-                        Cookies.remove('accessToken');
-                        return;
-                    }
-                    throw new Error(res.statusText);
-                }
-                return res.json();
-            }).then(currentUser =>
-                this.setState({
-                    currentUser
-                })
-            );
-        }
-    }
 
     render() {
-        if (!this.state.currentUser) {
-            return <LoginPage />;
-        }
-
-        return <QuestionPage />;
+        return(
+            <Router>
+                <Route exact path="/" component={Home} />
+            </Router>
+        );
     }
 }
 
-export default App;
+const mapStateToProps = state => ({
+    view: state.view
+});
+
+export default connect(mapStateToProps)(App);
---
+++ client/src/reducer.js
@@ -0,0 +1,7 @@
+const initialState = {
+  view:'home'
+};
+
+export const reducer = (state=initialState, action) => {
+  return state;
+};
