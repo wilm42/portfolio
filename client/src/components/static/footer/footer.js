a2165e9da4f3088f802820ddb5d15f0877fea0c9
williamtwobit <william@twobit.cc>
2017-09-13 17:00:02 -0400

    trying something different with footer images

--- client/src/components/static/footer/footer.js
+++ client/src/components/static/footer/footer.js
@@ -14,10 +14,10 @@ export class Footer extends React.Component{
         <div>handcrafted by william martin © 2017 | ufo icon created by Kaylen from the <a href="https://nounproject.org">noun project</a>.</div>
         <div className="links container">
           <a href="https://www.linkedin.com/in/william-martin-220667104/">
-            <img alt="footer link to linkedIn" src="./assets/linkedin-footer.svg"/>
+            <img src="./assets/linkedin-footer.svg" alt="linkedIn" />
           </a>
           <a href="https://github.com/williamtwobit">
-            <img alt="footer link to gitHub" src="./assets/github-footer.svg"/>
+            <img src="./assets/github-footer.svg" alt="gitHub" />
           </a>
         </div>
       </footer>
