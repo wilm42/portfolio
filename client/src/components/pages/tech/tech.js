18868437abc5653770c1935041fb16ed9ff18223
williamtwobit <william@twobit.cc>
2017-09-14 20:55:54 -0400

    added staggered animations to tech logos

--- client/src/components/pages/tech/tech.js
+++ client/src/components/pages/tech/tech.js
@@ -23,53 +23,53 @@ export class Tech extends React.Component{
             <div className="software container">
               <div className="row">
                 <div className="logo">
-                  <img src="./assets/tech-logos/react.svg" />
+                  <img src="./assets/tech-logos/react.svg"  style={{animationDelay:'.1s'}}/>
                 </div>
                 <div className="logo">
-                  <img src="./assets/tech-logos/javascript.svg" />
+                  <img src="./assets/tech-logos/javascript.svg" style={{animationDelay:'.2s'}}/>
                 </div>
                 <div className="logo">
-                  <img src="./assets/tech-logos/sketch-2.svg" />
+                  <img src="./assets/tech-logos/sketch-2.svg" style={{animationDelay:'.3s'}}/>
                 </div>
                 <div className="logo">
-                  <img src="./assets/tech-logos/Git.svg" />
+                  <img src="./assets/tech-logos/Git.svg" style={{animationDelay:'.4s'}}/>
                 </div>
                 <div className="logo">
-                  <img src="./assets/tech-logos/adobe-illustrator-cc.svg" />
+                  <img src="./assets/tech-logos/adobe-illustrator-cc.svg" style={{animationDelay:'.5s'}} />
                 </div>
               </div>
               <div className="row middle">
                 <div className="logo">
-                  <img src="./assets/tech-logos/redux.svg" />
+                  <img src="./assets/tech-logos/redux.svg" style={{animationDelay:'.15s'}} />
                 </div>
                 <div className="logo">
-                  <img src="./assets/tech-logos/Node.svg" />
+                  <img src="./assets/tech-logos/Node.svg" style={{animationDelay:'.25s'}} />
                 </div>
                 <div className="logo">
-                  <img src="./assets/tech-logos/bootstrap-4.svg" />
+                  <img src="./assets/tech-logos/bootstrap-4.svg" style={{animationDelay:'.35s'}} />
                 </div>
                 <div className="logo">
-                  <img src="./assets/tech-logos/zeplin.svg" />
+                  <img src="./assets/tech-logos/zeplin.svg" style={{animationDelay:'.45s'}} />
                 </div>
                 <div className="logo">
-                  <img src="./assets/tech-logos/mongodb.svg" />
+                  <img src="./assets/tech-logos/mongodb.svg" style={{animationDelay:'.55s'}} />
                 </div>
               </div>
               <div className="row">
                 <div className="logo">
-                  <img src="./assets/tech-logos/python-5.svg" />
+                  <img src="./assets/tech-logos/python-5.svg" style={{animationDelay:'.155s'}} />
                 </div>
                 <div className="logo">
-                  <img src="./assets/tech-logos/django.svg" />
+                  <img src="./assets/tech-logos/django.svg" style={{animationDelay:'.255s'}} />
                 </div>
                 <div className="logo">
-                  <img src="./assets/tech-logos/postgresql.svg" />
+                  <img src="./assets/tech-logos/postgresql.svg" style={{animationDelay:'.355s'}} />
                 </div>
                 <div className="logo">
-                  <img src="./assets/tech-logos/github-icon.svg" />
+                  <img src="./assets/tech-logos/github-icon.svg" style={{animationDelay:'.455s'}} />
                 </div>
                 <div className="logo">
-                  <img src="./assets/tech-logos/photoshop-cc.svg" />
+                  <img src="./assets/tech-logos/photoshop-cc.svg" style={{animationDelay:'.555s'}} />
                 </div>
               </div>
             </div>
