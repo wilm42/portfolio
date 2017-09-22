d3f5d2a0aacc4f5e4ce6303691c568d2e18eab9f
williamtwobit <william@twobit.cc>
2017-09-22 14:30:37 -0400

    two projects added to work section

--- client/src/components/pages/work/work.js
+++ client/src/components/pages/work/work.js
@@ -18,23 +18,23 @@ export class Work extends React.Component{
       <main className="work container">
         <Header text={"work"} />
         <div className="work-cards container">
+          
           <div className="work-card">
             <div className="gradient container">
               <img className="gradient-bg" src="./assets/work/work-card-bg.svg" />
-              <img className="work-top work-logo" src="./assets/work/logos/devCupid.svg" />
+              <img className="work-top work-logo" src="./assets/work/logos/seekhana.svg" />
               <div className="work-top work-synop container">
                 <p>
-                  short description goes here, lorem lorem blah blah blah
-                  short description goes here, lorem lorem blah blah blah
-                  short description goes here, lorem lorem blah blah blah
+                  seekhana is an app designed to teach basic Hindi to a western audience.
+                  My responsibilities on this app: Web Design, CSS, Algorithm design, and Node.
                 </p>
                 <div className="project-links">
-                  <a href="">source</a> | <a href="">live demo</a> | <a href="">detail</a>
+                  <a href="https://github.com/williamtwobit/seekhana">source</a> | <a href="http://seekhana.herokuapp.com">live demo</a>
                 </div>
               </div>
             </div>
             <div className="title container">
-              <img className="work-top" src="./assets/work/titles/devCupid.svg" />
+              <img className="work-top" src="./assets/work/titles/seekhana.svg" />
             </div>
           </div>
           <div className="work-card">
@@ -43,50 +43,11 @@ export class Work extends React.Component{
               <img className="work-top work-logo" src="./assets/work/logos/devCupid.svg" />
               <div className="work-top work-synop container">
                 <p>
-                  short description goes here, lorem lorem blah blah blah
-                  short description goes here, lorem lorem blah blah blah
-                  short description goes here, lorem lorem blah blah blah
+                  devCupid is a social app for finding project collaborators.
+                  My responsibilities on this app: Web Design, CSS, Product design, and React.
                 </p>
                 <div className="project-links">
-                  <a href="">source</a> | <a href="">live demo</a> | <a href="">detail</a>
-                </div>
-              </div>
-            </div>
-            <div className="title container">
-              <img className="work-top" src="./assets/work/titles/devCupid.svg" />
-            </div>
-          </div>
-          <div className="work-card">
-            <div className="gradient container">
-              <img className="gradient-bg" src="./assets/work/work-card-bg.svg" />
-              <img className="work-top work-logo" src="./assets/work/logos/devCupid.svg" />
-              <div className="work-top work-synop container">
-                <p>
-                  short description goes here, lorem lorem blah blah blah
-                  short description goes here, lorem lorem blah blah blah
-                  short description goes here, lorem lorem blah blah blah
-                </p>
-                <div className="project-links">
-                  <a href="">source</a> | <a href="">live demo</a> | <a href="">detail</a>
-                </div>
-              </div>
-            </div>
-            <div className="title container">
-              <img className="work-top" src="./assets/work/titles/devCupid.svg" />
-            </div>
-          </div>
-          <div className="work-card">
-            <div className="gradient container">
-              <img className="gradient-bg" src="./assets/work/work-card-bg.svg" />
-              <img className="work-top work-logo" src="./assets/work/logos/devCupid.svg" />
-              <div className="work-top work-synop container">
-                <p>
-                  short description goes here, lorem lorem blah blah blah
-                  short description goes here, lorem lorem blah blah blah
-                  short description goes here, lorem lorem blah blah blah
-                </p>
-                <div className="project-links">
-                  <a href="">source</a> | <a href="">live demo</a> | <a href="">detail</a>
+                  <a href="https://github.com/thinkful-c11/devcupid">source</a> | <a href="http://devcupid.herokuapp.com">live demo</a>
                 </div>
               </div>
             </div>
