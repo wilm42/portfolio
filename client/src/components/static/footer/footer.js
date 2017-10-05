1e6495dd53246fd0c9c43385ec1f83206fae41c1
wilm <william@twobit.cc>
2017-10-05 00:08:22 -0400

    prettier-ified footer

--- client/src/components/static/footer/footer.js
+++ client/src/components/static/footer/footer.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import './footer.css';
+import "./footer.css";
 
-export class Footer extends React.Component{
-  render(){
-    let style;
-    if(this.props.view === 'home'){ 
-      style= {opacity: '0', height:'0px'};
-    }
-    return(
-      <footer style={style}>
-        <div>handcrafted by william martin © 2017 | ufo icon created by Kaylen from the <a href="https://nounproject.org">noun project</a>.</div>
-        <div className="links container">
-          <a href="https://www.linkedin.com/in/william-martin-220667104/">
-            <img src="./assets/footer/li-logo-footer.svg" alt="linkedIn" />
-          </a>
-          <a href="https://github.com/williamtwobit">
-            <img src="./assets/footer/gh-logo-footer.svg" alt="gitHub" />
-          </a>
-        </div>
-      </footer>
-    );
-  }
+export class Footer extends React.Component {
+	render() {
+		let style;
+		if (this.props.view === "home") {
+			style = { opacity: "0", height: "0px" };
+		}
+		return (
+			<footer style={style}>
+				<div>
+					handcrafted by william martin © 2017 | ufo icon created by Kaylen from
+					the <a href="https://nounproject.org">noun project</a>.
+				</div>
+				<div className="links container">
+					<a href="https://www.linkedin.com/in/william-martin-220667104/">
+						<img src="./assets/footer/li-logo-footer.svg" alt="linkedIn" />
+					</a>
+					<a href="https://github.com/williamtwobit">
+						<img src="./assets/footer/gh-logo-footer.svg" alt="gitHub" />
+					</a>
+				</div>
+			</footer>
+		);
+	}
 }
 
-const mapStateToProps = (state) => ({
-  view: state.view
-})
+const mapStateToProps = state => ({
+	view: state.view,
+});
 
 export default connect(mapStateToProps)(Footer);
