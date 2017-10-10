3d013da6c9975c71e7012e2a792943ce3e6258dc
wilm <william@twobit.cc>
2017-10-10 10:11:10 -0400

    add anchor names to project grids

--- client/src/components/pages/projects/project.js
+++ client/src/components/pages/projects/project.js
@@ -3,75 +3,77 @@ import React from "react";
 export default class Project extends React.Component {
 	render() {
 		return (
-			<div className="project-grid">
-				{this.props.data.type === "WEB" ? (
-					<div className="mockup laptop-container">
-						<img
-							alt="laptop mockup"
-							className="laptop"
-							src="./assets/work/laptop-container.svg"
-						/>
-						<img
-							alt={this.props.data.title + " screenshot"}
-							className="screenShot laptop"
-							src={this.props.data.screenshot}
-						/>
-					</div>
-				) : (
-					<div className="mockup phone-container">
-						<img
-							className="phone-mock-bg"
-							alt=""
-							src="./assets/work/logos/seekhana.svg"
-						/>
-						<div className="pos-fix">
+			<a name={this.props.data.title.split(" ").join("")}>
+				<div className="project-grid">
+					{this.props.data.type === "WEB" ? (
+						<div className="mockup laptop-container">
 							<img
-								alt="phone mockup"
-								className="phone"
-								src="./assets/work/phone-container.svg"
+								alt="laptop mockup"
+								className="laptop"
+								src="./assets/work/laptop-container.svg"
 							/>
 							<img
 								alt={this.props.data.title + " screenshot"}
-								className="screenShot phone"
+								className="screenShot laptop"
 								src={this.props.data.screenshot}
 							/>
 						</div>
-					</div>
-				)}
+					) : (
+						<div className="mockup phone-container">
+							<img
+								className="phone-mock-bg"
+								alt=""
+								src="./assets/work/logos/seekhana.svg"
+							/>
+							<div className="pos-fix">
+								<img
+									alt="phone mockup"
+									className="phone"
+									src="./assets/work/phone-container.svg"
+								/>
+								<img
+									alt={this.props.data.title + " screenshot"}
+									className="screenShot phone"
+									src={this.props.data.screenshot}
+								/>
+							</div>
+						</div>
+					)}
 
-				<div className="project-links">
-					<div>
-						<a href={this.props.data.live}>live demo</a>
+					<div className="project-links">
+						<div>
+							<a href={this.props.data.live}>live demo</a>
+						</div>
+						<div className="sep"> | </div>
+						<div>
+							<a href={this.props.data.source}>source code</a>
+						</div>
 					</div>
-					<div className="sep"> | </div>
-					<div>
-						<a href={this.props.data.source}>source code</a>
+					<div className="project text">
+						<h2 className="project">{this.props.data.title}</h2>
+						<div className="body">
+							{this.props.data.description.map((paragraph, index) => {
+								return <p key={index}>{paragraph}</p>;
+							})}
+						</div>
 					</div>
-				</div>
-				<div className="project text">
-					<h2 className="project">{this.props.data.title}</h2>
-					<div className="body">
-						{this.props.data.description.map((paragraph, index) => {
-							return <p key={index}>{paragraph}</p>;
+
+					<div
+						className="project-tech"
+						style={
+							this.props.data.type === "MOBILE" ? { display: "none" } : null
+						}>
+						{this.props.data.tech.map(item => {
+							return (
+								<div key={item.text} className="project-logo">
+									<img alt={item.text} src={item.logo} />
+									<h3>{item.text}</h3>
+								</div>
+							);
 						})}
 					</div>
 				</div>
-
-				<div
-					className="project-tech"
-					style={
-						this.props.data.type === "MOBILE" ? { display: "none" } : null
-					}>
-					{this.props.data.tech.map(item => {
-						return (
-							<div key={item.text} className="project-logo">
-								<img alt={item.text} src={item.logo} />
-								<h3>{item.text}</h3>
-							</div>
-						);
-					})}
-				</div>
-			</div>
+			</a>
 		);
 	}
 }
