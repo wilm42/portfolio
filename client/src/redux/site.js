3a0f2ca21b969c9e0143a8df68a876d094f0391f
wilm <william@twobit.cc>
2017-10-10 14:31:22 -0400

    update email address

--- client/src/redux/site.js
+++ client/src/redux/site.js
@@ -4,7 +4,7 @@ import React from "react";
 export const wilm = {
 	nameCap: "Wilm",
 	nameLowr: "wilm",
-	email: "hi@wilm.tech",
+	email: "wilm@twobit.cc",
 	bioBlurb: `A quick note about my name: I'm currently in the process of transitioning
 			from the very classically masculine 'William' to the much more
 			gender-neutral 'Wilm' to reflect my identity as a non-binary person. As a
