076b9c3920265dbd5a5a4c06ef3c2570ab283058
wilm <william@twobit.cc>
2017-10-10 11:11:09 -0400

    fill in site.js

---
+++ client/src/redux/site.js
@@ -0,0 +1,22 @@
+export const site = {
+	william: {
+		nameCap: "William",
+		nameLowr: "william",
+		email: "hi@william.direct",
+		bioBlurb: null,
+	},
+	wilm: {
+		nameCap: "Wilm",
+		nameLowr: "wilm",
+		email: "hi@wilm.tech",
+		bioBlurb: (
+			<p>
+				A quick note about my name: I'm currently in the process of
+				transitioning from the very classically masculine 'William' to the much
+				more gender-neutral 'Wilm' to reflect my identity as a non-binary
+				person. As a result, you may see both around the internet - but I do
+				prefer Wilm.
+			</p>
+		),
+	},
+};
